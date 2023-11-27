670fbc5967aa4d265d7b7f38c0181c45fd23a7a6
Alex Lee <3076438032@qq.com>
2023-11-27 23:53:27 +0800

    docs: 2023.11.27

--- 手写JS代码/promise.js
+++ 手写JS代码/promise.js
@@ -27,10 +27,14 @@ class MyPromise {
     while (this.rejectCallbackArray.length) this.rejectCallbackArray.shift()()
   }
   // 链式调用方法
-  then (successCallback, failCallback) {
+  then(successCallback, failCallback) {
     // 保证存在成功函数和失败函数
     successCallback = successCallback ? successCallback : (value) => value
-    failCallback = failCallback ? failCallback : (err) => { throw err }
+    failCallback = failCallback
+      ? failCallback
+      : (err) => {
+          throw err
+        }
     // 定义处理
     let myPromise = new MyPromise((resolve, reject) => {
       switch (this.PromiseStatus) {
@@ -48,7 +52,6 @@ class MyPromise {
               } catch (err) {
                 reject(err)
               }
-
             }, 0)
           )
           this.rejectCallbackArray.push(() => {
@@ -68,8 +71,10 @@ class MyPromise {
           try {
             setTimeout(() => {
               resolvePromise(
-                promise, successCallback(this.PromiseValue),
-                resolve, reject
+                promise,
+                successCallback(this.PromiseValue),
+                resolve,
+                reject
               )
             }, 0)
           } catch (err) {
@@ -79,7 +84,12 @@ class MyPromise {
         case 'rejected':
           try {
             setTimeout(() => {
-              resolvePromise(promise, failCallback(this.PromiseError), resolve, reject)
+              resolvePromise(
+                promise,
+                failCallback(this.PromiseError),
+                resolve,
+                reject
+              )
             }, 0)
           } catch (err) {
             reject(err)
@@ -91,77 +101,108 @@ class MyPromise {
     return myPromise
   }
   // 失败调用方法
-  catch (failCallback) {
+  catch(failCallback) {
     this.then(undefined, failCallback)
   }
   //finally方法，无论成功还是失败，都会执行该方法
-  finally (finalCallback) {
-    return this.then((value) => {
-      //再增加一个then是为了考虑回调函数是一个异步调用的promise的情况
-      return MyPromise.resolve(finalCallback()).then(() => value)
-    }, (err) => {
-      return MyPromise.resolve(finalCallback()).then(() => { throw err })
-    }
+  finally(finalCallback) {
+    return this.then(
+      (value) => {
+        //再增加一个then是为了考虑回调函数是一个异步调用的promise的情况
+        return MyPromise.resolve(finalCallback()).then(() => value)
+      },
+      (err) => {
+        return MyPromise.resolve(finalCallback()).then(() => {
+          throw err
+        })
+      }
     )
   }
   //前置包装函数
-  function resolvePromise (promise, value, resolve, reject) {
-  // 不允许返回promise自身
-  if (promise === value) {
-    //return作用是为了中断执行
-    return TypeError("Chaining cycle detected for promise #<Promise>")
-  } else if (value instanceof MyPromise) {
-    value.then(resolve, reject)
-  } else {
-    resolve(value)
+  resolvePromise(promise, value, resolve, reject) {
+    // 不允许返回promise自身
+    if (promise === value) {
+      //return作用是为了中断执行
+      return TypeError('Chaining cycle detected for promise #<Promise>')
+    } else if (value instanceof MyPromise) {
+      value.then(resolve, reject)
+    } else {
+      resolve(value)
+    }
   }
-}
   /**
    * Promise.all实现
-   * 等待数组中的所有Promise全部执行完毕才返回结果
+   * 等待数组中的所有Promise全部执行完毕且都成功才返回结果
    * @param {Array} array 传入Promise可迭代对象
    * @returns {Function} Promise
    */
-  static all(array){
-  return new MyPromise((resolve, reject) => {
-    let result = new Array(array.length)
-    let count = 0
-    let setResolve = (count, length, result) => {
-      if (count === length) resolve(result)
-    }
-    for (let i = 0; i < array.length: i++){
-    //参数为函数
-    if (array[i] instanceof Function) {
-      result[i] = array[i]()
-      count++
-      setResolve(count, array.length, result)
-      //参数为MyPromise的对象
-    } else if (array[i] instanceof MyPromise) {
-      array[i].then((value) => {
-        result[i] = value
-        count++
-        setResolve(count, array.length, result)
-      }, (err) => {
-        reject(err)
-      })
-      //其他类型参数
-    } else {
-      result[i] = array[i]
-      count++
-      setResolve(count, array.length, result)
-    }
+  static all(array) {
+    return new MyPromise((resolve, reject) => {
+      let result = new Array(array.length)
+      let count = 0
+      let setResolve = (count, length, result) => {
+        if (count === length) resolve(result)
+      }
+      for (let i = 0; i < array.length; i++) {
+        //参数为函数
+        if (array[i] instanceof Function) {
+          result[i] = array[i]()
+          count++
+          setResolve(count, array.length, result)
+          //参数为MyPromise的对象
+        } else if (array[i] instanceof MyPromise) {
+          array[i].then(
+            (value) => {
+              result[i] = value
+              count++
+              setResolve(count, array.length, result)
+            },
+            (err) => {
+              reject(err)
+            }
+          )
+          //其他类型参数
+        } else {
+          result[i] = array[i]
+          count++
+          setResolve(count, array.length, result)
+        }
+      }
+    })
   }
-})
+  /**
+   * Promise.allSettled实现
+   * 无论成功和失败，所有promise都执行，并且返回一个状态表
+   */
+  static allSettled(array) {
+    const results = []
+    const len = array.length
+    let resolvedCount = 0
+    for (let i = 0; i < len; i++) {
+      array[i]
+        .then((value) => {
+          results[i] = { status: 'fulfilled', value }
+        })
+        .catch((reason) => {
+          results[i] = { status: 'rejected', reason }
+        })
+        .finally(() => {
+          resolvedCount++
+          if (resolvedCount === len) {
+            this.resolve(results)
+          }
+        })
+    }
   }
   /**
    * Promise.resolve实现
    */
-  static resolve(value){
-  // value为Promise直接返回
-  if (value instanceof MyPromise) return value
-  // value为值则代表该值为promise兑现值
-  return new MyPromise((resolve, reject) => {
-    resolve(value)
-  })
-}
+  static resolve(value) {
+    // value为Promise直接返回
+    if (value instanceof MyPromise) return value
+    // value为值则代表该值为promise兑现值
+    return new MyPromise((resolve, reject) => {
+      resolve(value)
+    })
+  }
 }
