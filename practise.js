b19237cc3af0b84ebe1a71ffe7ee5e2ddc7026ce
Alex Lee <3076438032@qq.com>
2023-12-11 18:12:43 +0800

    docs: 2023.12.11

--- practise.js
+++ practise.js
@@ -1,26 +1,21 @@
 function throttle(fn, interval, leading = true, trailing = false) {
-  let timer = null
   let lastTime = 0
+  let timer = null
   let isLeading = true
 
-  return (...args) => {
+  return function (...args) {
     const nowTime = new Date().getTime()
 
-    const remainTime = nowTime - lastTime
-
-    if (!leading && isLeading) {
+    if (isLeading && !leading) {
       lastTime = nowTime
       isLeading = false
     }
-
-    if (remainTime - interval >= 0) {
+    const remainTime = nowTime - lastTime
+    if (remainTime >= interval) {
       fn.apply(this, args)
       lastTime = nowTime
-    }
-
-    if (remainTime < interval) {
+    } else {
       if (timer) clearTimeout(timer)
-
       timer = setTimeout(() => {
         if (trailing) {
           fn.apply(this, args)
--- 手写JS代码/deepClone.js
+++ 手写JS代码/deepClone.js
@@ -1,4 +1,5 @@
 //https://segmentfault.com/a/1190000020255831
+
 function deepClone(obj, hash = new WeakMap()) {
   if (obj === null) return obj
   if (obj instanceof Date) return new Date(obj)
@@ -8,6 +9,7 @@ function deepClone(obj, hash = new WeakMap()) {
 
   //解决循环引用：额外开辟一个空间，存储当前对象和拷贝对象的对应关系，当需要拷贝当前对象时，先去存储空间找，有没有拷贝过这个对象，如果有直接返回，如果没有继续拷贝。
   if (hash.get(obj)) return hash.get(obj)
+
   let cloneObj = new obj.constructor()
   hash.set(obj, cloneObj)
   for (let key in obj) {
