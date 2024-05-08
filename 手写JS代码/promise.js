1aa793f10f2ebcd6a1d7121222bea63e9aa3faa0
Alex Lee <3076438032@qq.com>
2024-05-08 22:10:19 +0800

    doc: 2024.5.8

--- 手写JS代码/promise.js
+++ 手写JS代码/promise.js
@@ -1,51 +1,72 @@
-//手写promise
-//包含四个api：new Promise().catch() new Promise().finally() Promise.all() Promise.resolve()
-class MyPromise {
-  // 构造函数：传入resolve和reject函数
-  constructor(executor) {
-    executor(this.resolve, this.reject)
-  }
-  PromiseValue //promise内置value
-  PromiseStatus = 'pending' //promise状态分别为pending、rejected、resolved，默认状态pending
-  PromiseError //promise内置error
-  resolveCallbackArray = [] //收集resolve的回调函数集合
-  rejectCallbackArray = [] //收集reject的回调函数集合
+// 实例方法API: then,catch,finally
+// 静态方法API: all,,race,resolve,reject,allSettled
 
-  // 成功回调函数
+class MyPromise {
+  // 构造函数：接收一个executor函数（即new Promise传入的函数），executor函数接收两个参数，分别是resolve和reject函数
+  constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new Error('MyPromise must accept a function as a parameter')
+    }
+
+    // promise传入函数直接失败则直接进入reject
+    try {
+      executor(this.resolve, this.reject)
+    } catch (error) {
+      this.reject(error)
+    }
+  }
+
+  // promise状态
+  PromiseStatus = 'pending'
+  PromiseValue
+  PromiseError
+
+  // 回调函数集合
+  // 为什么需要它们：因为promise的状态是不可逆的（直接.then时promise状态可能还没发生改变，那么执行then是不合理的），所以需要收集resolve和reject的回调函数，以便在状态改变时执行
+  onResolvedCallbacks = [] //收集resolve的回调函数集合
+  onRejectedCallbacks = [] //收集reject的回调函数集合
+
+  // 成功回调方法
   resolve = (value) => {
     if (this.PromiseStatus !== 'pending') return
+
+    this.PromiseStatus = 'fulfilled'
     this.promiseValue = value
-    this.PromiseStatus = 'resolved'
-    while (this.resolveCallbackArray.length) this.resolveCallbackArray.shift()()
+
+    while (this.onResolvedCallbacks.length) this.onResolvedCallbacks.shift()()
   }
 
-  // 失败回调函数
+  // 失败回调方法
   reject = (error) => {
     if (this.PromiseStatus !== 'pending') return
-    this.PromiseError = error
+
     this.PromiseStatus = 'rejected'
-    while (this.rejectCallbackArray.length) this.rejectCallbackArray.shift()()
+    this.PromiseError = error
+
+    while (this.onRejectedCallbacks.length) this.onRejectedCallbacks.shift()()
   }
+
   // 链式调用方法
-  then(successCallback, failCallback) {
-    // 保证存在成功函数和失败函数
-    successCallback = successCallback ? successCallback : (value) => value
-    failCallback = failCallback
-      ? failCallback
+  then(onFulfilled, onRejected) {
+    // 默认处理方式
+    onFulfilled = onFulfilled ? onFulfilled : (value) => value
+    onRejected = onRejected
+      ? onRejected
       : (err) => {
           throw err
         }
-    // 定义处理
+
+    // 为了在promise实例内部拿到该实例自身，需要使用setTimeout处理，因为promise在微任务执行完毕之后才会执行宏任务中的setTimeout，这样setTimeout中的代码就能拿到promise实例自身
     let myPromise = new MyPromise((resolve, reject) => {
       switch (this.PromiseStatus) {
+        // 状态没改变时，将回调函数收集起来
         case 'pending':
-          this.resolveCallbackArray.push(
-            // 为了在promise实例内部拿到该实例自身，需要使用setTimeout处理，因为promise在微任务执行完毕之后才会执行宏任务中的setTimeout
+          this.onResolvedCallbacks.push(() => {
             setTimeout(() => {
               try {
                 resolvePromise(
                   myPromise,
-                  successCallback(this.promiseValue),
+                  onFulfilled(this.promiseValue),
                   resolve,
                   reject
                 )
@@ -53,12 +74,12 @@ class MyPromise {
                 reject(err)
               }
             }, 0)
-          )
-          this.rejectCallbackArray.push(() => {
+          })
+          this.onRejectedCallbacks.push(() => {
             setTimeout(() => {
               try {
                 resolvePromise(() => {
-                  myPromise, failCallback(this.PromiseValue)
+                  myPromise, onRejected(this.PromiseError)
                   resolve, reject
                 })
               } catch (err) {
@@ -67,12 +88,12 @@ class MyPromise {
             }, 0)
           })
           break
-        case 'resolved':
+        case 'fulfilled':
           try {
             setTimeout(() => {
               resolvePromise(
-                promise,
-                successCallback(this.PromiseValue),
+                myPromise,
+                onFulfilled(this.PromiseValue),
                 resolve,
                 reject
               )
@@ -85,8 +106,8 @@ class MyPromise {
           try {
             setTimeout(() => {
               resolvePromise(
-                promise,
-                failCallback(this.PromiseError),
+                myPromise,
+                onRejected(this.PromiseError),
                 resolve,
                 reject
               )
@@ -100,10 +121,13 @@ class MyPromise {
     })
     return myPromise
   }
+
   // 失败调用方法
+  // catch相比then的failCallback更强，failCallback只能捕获promise的失败，而catch可以捕获promise.then内部执行代码的错误
   catch(failCallback) {
     this.then(undefined, failCallback)
   }
+
   //finally方法，无论成功还是失败，都会执行该方法
   finally(finalCallback) {
     return this.then(
@@ -118,11 +142,11 @@ class MyPromise {
       }
     )
   }
-  //前置包装函数
+
+  // then的前置处理方法
   resolvePromise(promise, value, resolve, reject) {
-    // 不允许返回promise自身
-    if (promise === value) {
-      //return作用是为了中断执行
+    // 不允许返回promise自身，会导致循环调用
+    if (value === promise) {
       return TypeError('Chaining cycle detected for promise #<Promise>')
     } else if (value instanceof MyPromise) {
       value.then(resolve, reject)
@@ -130,42 +154,40 @@ class MyPromise {
       resolve(value)
     }
   }
+
   /**
    * Promise.all实现
    * 等待数组中的所有Promise全部执行完毕且都成功才返回结果
-   * @param {Array} array 传入Promise可迭代对象
+   * @param {Array} promises 传入Promise可迭代对象
    * @returns {Function} Promise
    */
-  static all(array) {
+  static all(promises) {
     return new MyPromise((resolve, reject) => {
-      let result = new Array(array.length)
+      let result = new Array(promises.length)
       let count = 0
-      let setResolve = (count, length, result) => {
-        if (count === length) resolve(result)
+
+      let setResolve = (count, result) => {
+        count++
+        if (count === promises.length) resolve(result)
       }
-      for (let i = 0; i < array.length; i++) {
-        //参数为函数
-        if (array[i] instanceof Function) {
-          result[i] = array[i]()
-          count++
-          setResolve(count, array.length, result)
-          //参数为MyPromise的对象
-        } else if (array[i] instanceof MyPromise) {
-          array[i].then(
+
+      for (let i = 0; i < promises.length; i++) {
+        if (promises[i] instanceof Function) {
+          result[i] = promises[i]()
+          setResolve(count, result)
+        } else if (promises[i] instanceof MyPromise) {
+          promises[i].then(
             (value) => {
               result[i] = value
-              count++
-              setResolve(count, array.length, result)
+              setResolve(count, result)
             },
             (err) => {
               reject(err)
             }
           )
-          //其他类型参数
         } else {
-          result[i] = array[i]
-          count++
-          setResolve(count, array.length, result)
+          result[i] = promises[i]
+          setResolve(count, result)
         }
       }
     })
@@ -174,12 +196,12 @@ class MyPromise {
    * Promise.allSettled实现
    * 无论成功和失败，所有promise都执行，并且返回一个状态表
    */
-  static allSettled(array) {
+  static allSettled(promises) {
     const results = []
-    const len = array.length
+    const len = promises.length
     let resolvedCount = 0
     for (let i = 0; i < len; i++) {
-      array[i]
+      promises[i]
         .then((value) => {
           results[i] = { status: 'fulfilled', value }
         })
@@ -198,23 +220,32 @@ class MyPromise {
    * Promise.resolve实现
    */
   static resolve(value) {
-    // value为Promise直接返回
     if (value instanceof MyPromise) return value
-    // value为值则代表该值为promise兑现值
+
     return new MyPromise((resolve, reject) => {
       resolve(value)
+    })
+  }
+  /**
+   * Promise.reject实现
+   */
+  static resolve(value) {
+    if (value instanceof MyPromise) return value
+
+    return new MyPromise((resolve, reject) => {
+      reject(value)
     })
   }
   /**
    * Promise.race实现
    * 只要有一个promise rejected/fulfilled返回的promise就会rejected/fulfilled
    */
-  static race(array) {
+  static race(promises) {
     let hasValue = false
     let hasError = false
     return new MyPromise((resolve, reject) => {
-      for (let i = 0; i < array.length; i++) {
-        array[i].then(
+      for (let i = 0; i < promises.length; i++) {
+        promises[i].then(
           (data) => {
             !hasValue && !hasError && resolve(data)
             hasValue = true
@@ -229,10 +260,19 @@ class MyPromise {
   }
 }
 
+// Test process
+
+// promise三个状态：pending、fulfilled、rejected
+// resolve是成功调用的函数(并将状态pending->fulfilled)，reject是失败调用的函数(并将状态pending->rejected)
+// promise内部一般会封装一个异步函数，异步函数执行完毕后调用resolve或reject
+// resolve和reject函数只能调用一次，且调用后promise的状态不可逆
 const fn = new MyPromise((resolve, reject) => {
-  resolve(123)
+  // 这里的代码是立刻执行的
+  setTimeout(() => {
+    resolve(123)
+  }, 2000)
 })
-fn.then((success, error) => {
-  success()
-  // console.log('success')
+
+fn.then((res) => {
+  console.log(res) // 123
 })
